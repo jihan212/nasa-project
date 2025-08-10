84b496befd2173cc8b3f4090a19ff12faa2eb075
jihan212 <jihanbjashim@gmail.com>
2025-08-11 01:06:52 +0600

    Implement launch data fetching and submission; enhance error handling in HTTP requests, and update asset manifest with new build files.

--- client/src/hooks/useLaunches.js
+++ client/src/hooks/useLaunches.js
@@ -18,21 +18,20 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
 	const submitLaunch = useCallback(
 		async (e) => {
 			e.preventDefault();
-			// setPendingLaunch(true);
+			setPendingLaunch(true);
 			const data = new FormData(e.target);
 			const launchDate = new Date(data.get('launch-day'));
 			const mission = data.get('mission-name');
 			const rocket = data.get('rocket-name');
 			const target = data.get('planets-selector');
-			await httpSubmitLaunch({
+			const response = await httpSubmitLaunch({
 				launchDate,
 				mission,
 				rocket,
 				target,
 			});
 
-			// TODO: Set success based on response.
-			const success = false;
+			const success = response.ok;
 			if (success) {
 				getLaunches();
 				setTimeout(() => {
