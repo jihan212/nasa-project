65943bf967f406a9ac2a96748c844ef5424bc563
jihan212 <jihanbjashim@gmail.com>
2025-08-31 18:45:24 +0600

    Remove package-lock.json and add MongoDB and Mongoose dependencies; establish MongoDB connection in server.js for data management.

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,27 @@
-const express = require('express');
 const http = require('http');
+const mongoose = require('mongoose');
+
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = `mongodb+srv://root:takdir9095@bot.txta0.mongodb.net/?retryWrites=true&w=majority&appName=bot`;
+
 const server = http.createServer(app);
 
+mongoose
+	.connect(MONGO_URL)
+	.then(() => {
+		console.log('MongoDB connection ready!');
+	})
+	.catch((err) => {
+		console.error(err);
+	});
+
 async function startServer() {
+	await mongoose.connect(MONGO_URL);
+
 	await loadPlanetsData();
 
 	server.listen(PORT, () => {
